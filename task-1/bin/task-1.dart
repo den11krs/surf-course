f25d998e4e7bb3ca40f8718669b7b536059a5cda
Ilya Vasilyev <den11krs@gmail.com>
2023-09-04 13:06:33 +0700

    Do task-1 revision

--- task-1/bin/task-1.dart
+++ task-1/bin/task-1.dart
@@ -1,16 +1,14 @@
 import 'package:task_1/map_before_2010.dart';
 import 'package:task_1/map_after_2010.dart';
-import 'package:task_1/calculate_average_age.dart';
-import 'package:task_1/calculate_average_age_of_half.dart';
+import 'package:task_1/calculate_average_ages.dart';
 
 void main() {
   List<Map> incomingData = [mapBefore2010, mapAfter2010];
 
-  num averageAge = calculateAverageAge(incomingData);
-  print('Средний возраст всей техники равен ${averageAge.toStringAsFixed(2)}.');
-  print('');
+  final List<num> averageAges = calculateAverageAges(incomingData);
 
-  num averageAgeOfHalf = calculateAverageAgeOfHalf(incomingData);
   print(
-      'Средний возраст 50% самой старой техники равен ${averageAgeOfHalf.toStringAsFixed(2)}.');
+      'Средний возраст всей техники равен ${averageAges[0].toStringAsFixed(2)}.');
+  print(
+      'Средний возраст 50% самой старой техники равен ${averageAges[1].toStringAsFixed(2)}.');
 }
